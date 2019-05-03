5c49346c051597b1647fa10bb491334b89806327
Oliver Stein <oliver.stein.privat@gmail.com>
2019-05-03 15:48:25 +0200

    update tls, password hasher and added extra volume claims

--- pharmbio/pharmbio-notebook/jupyter-hashpass-generator.sh
+++ pharmbio/pharmbio-notebook/jupyter-hashpass-generator.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 set -e
-
-password="$1"
+echo "Enter password: "
+read password
 salt=$(openssl rand -hex 6)
 hashed_passw=$(echo -n "$password$salt" | openssl dgst -sha1 | sed 's/^.* //')
 jupyter_format_hash="sha1:$salt:$hashed_passw"
