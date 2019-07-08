92b57ae595e9940b232d7044cac83d8b703dc846
Oliver Stein <oliver.stein.privat@gmail.com>
2019-07-08 15:47:14 +0200

    removed pharmbio, moved index to top

--- pharmbio/charts/pharmbio-notebook/jupyter-hashpass-generator.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-echo "Enter password: "
-read password
-salt=$(openssl rand -hex 6)
-hashed_passw=$(echo -n "$password$salt" | openssl dgst -sha1 | sed 's/^.* //')
-jupyter_format_hash="sha1:$salt:$hashed_passw"
-echo $jupyter_format_hash
